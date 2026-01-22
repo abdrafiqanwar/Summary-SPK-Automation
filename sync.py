b54e71e15d78b41806db1066c1ad25579ff075d1
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2026-01-22 15:52:59 +0800

    feat: google sheets sync

---
+++ sync.py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+from google.oauth2.service_account import Credentials
+import pandas as pd
+import os, gspread, warnings
+
+warnings.filterwarnings("ignore", message="Workbook contains no default style*")
+
+load_dotenv()
+
+download_path = os.getenv("DOWNLOAD_PATH")
+spreadsheet_id = os.getenv("SPREADSHEET_ID")
+
+files = [
+    os.path.join(download_path, f)
+    for f in os.listdir(download_path)
+    if f.endswith(".xls") and f.startswith("Laporan-Summary")
+]
+
+if not files:
+    print("File tidak ditemukan ⚠️")
+else:
+    latest_file = max(files, key=os.path.getctime)
+
+    df = pd.read_html(latest_file)
+    df = df[0]
+
+    scope = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive",
+    ]
+
+    creds = Credentials.from_service_account_file("credentials.json", scopes=scope)
+    client = gspread.authorize(creds)
+
+    spreadsheet = client.open_by_key(spreadsheet_id)
+    worksheet = spreadsheet.worksheet("SPK")
+
+    df = df.fillna("")
+
+    worksheet.clear()
+    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
